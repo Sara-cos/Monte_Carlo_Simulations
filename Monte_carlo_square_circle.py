ac8690da5c1b100c456ea90d7b31fe05639b7a2f
Thejas Bhat P <NHI483@nslhub.in>
2023-01-30 17:34:55 +0530

    comments_added

--- Monte_carlo_square_circle.py
+++ Monte_carlo_square_circle.py
@@ -10,25 +10,24 @@ def MC_Simualtion():
     Radius = 0.5 # Sample radius of 0.5
     Circle = 0
     Big_Square = 0
-   
-    for i in range(10000000): # 10000000 points in total for testing
+    Total_points = 10000000 # 10000000 points in total for testing
+
+    for i in range(Total_points): 
         x = np.random.uniform(-Radius,Radius) # Random points from -radius to radius to include all 4 quadrants
         y = np.random.uniform(-Radius,Radius) # of x and y
 
-        #Distance between the coodinate and origin should be <= 0.5
+        # Distance between the coodinate and origin should be <= 0.5
         Distance = (np.sqrt(y**2 + x**2)) # Distance of all the points from the origin
 
         if(Distance <= Radius): # Check if point lies inside or outside the circle
             Circle = Circle + 1
-        else:
-            Big_Square = Big_Square + 1
 
     # Take the ratio of circle with the points that lie outside the circle ie total number of points
-    Ratio_Big_Square = Circle/10000000
+    Ratio_Big_Square = Circle/Total_points
     print("Circle : {}".format(Circle))
-    print("Square : {}".format(10000000))
+    print("Square : {}".format(Total_points))
 
-    ## We get the Ratio_Big_Square as pi/4 so we multiply it by 4
+    ## We get the Ratio_Big_Square as pi/4 so we multiply it by 4, to get the pi value
 
     Ratio =  4*Ratio_Big_Square
 
